c01aa2a94d38bdbb103f6383877f0fec2b52025a
vishnu <ashwinvishnu123@gmail.com>
2026-09-11 12:27:39 +0400

    first commit

---
+++ Docker/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import socket
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return f"""
+    <p>Application deployed!</p>
+    <p>Hostname: {socket.gethostname()}</p>
+    """
+
+@app.route("/health")
+def health():
+    return "OK", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
---
+++ terraform/ec2.tf
@@ -0,0 +1,31 @@
+resource "aws_instance" "Myapp"{
+    ami = "ami-01a00762f46d584a1"
+    instance_type = "t3.small"
+    key_name = "devops-project"
+    security_groups = [aws_security_group.my_sg.name]
+
+    user_data = <<-EOF
+              #!/bin/bash
+              # Update system
+              sudo apt-get update -y
+
+              # Install Docker
+              sudo apt-get install -y docker.io
+              sudo systemctl start docker
+              sudo systemctl enable docker
+
+              # Install Jenkins
+              sudo apt-get install -y openjdk-11-jdk
+              wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+              sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+              sudo apt-get update -y
+              sudo apt-get install -y jenkins
+
+              # Start Jenkins
+              sudo systemctl start jenkins
+              sudo systemctl enable jenkins
+              EOF
+    tags = {
+        Name = "Myproject_app"
+    }
+}
